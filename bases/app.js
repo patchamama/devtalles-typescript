59d941e9fce23509a333401e4a77675d933094d2
A. Urquiola <patchamama.go@gmail.com>
2023-11-19 11:14:31 +0100

    Tarea y Resolución del Ejercicio #1

--- bases/app.js
+++ bases/app.js
@@ -1,7 +1,43 @@
 "use strict";
 ;
 (function () {
-    var a = 10;
-    var b;
-    console.log(a);
+    // Tipos
+    var batman = 'Bruce';
+    var superman = 'Clark';
+    var existe = false;
+    console.log(batman, superman, existe);
+    // Tuplas
+    var parejaHeroes = [batman, superman];
+    var villano = ['Lex Lutor', 5, true];
+    console.log({ parejaHeroes: parejaHeroes, villano: villano });
+    // Arreglos
+    var aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    console.log({ aliados: aliados });
+    //Enumeraciones
+    var fuerza;
+    (function (fuerza) {
+        fuerza[fuerza["acuaman"] = 0] = "acuaman";
+        fuerza[fuerza["batman"] = 1] = "batman";
+        fuerza[fuerza["flash"] = 5] = "flash";
+        fuerza[fuerza["superman"] = 100] = "superman";
+    })(fuerza || (fuerza = {}));
+    var fuerzaFlash = fuerza.flash;
+    var fuerzaSuperman = fuerza.superman;
+    var fuerzaBatman = fuerza.batman;
+    var fuerzaAcuaman = fuerza.acuaman;
+    console.log(fuerza);
+    // Retorno de funciones
+    function activar_batiseñal() {
+        return 'activada';
+    }
+    console.log(activar_batiseñal());
+    function pedir_ayuda() {
+        console.log('Auxilio!!!');
+    }
+    console.log(pedir_ayuda());
+    // Aserciones de Tipo
+    var poder = '100';
+    var largoDelPoder = poder.length;
+    var largoDelPoder1 = poder.length;
+    console.log({ poder: poder, largoDelPoder: largoDelPoder, largoDelPoder1: largoDelPoder1 });
 })();
--- bases/app.ts
+++ bases/app.ts
@@ -1,6 +1,46 @@
 ;(() => {
-  const a: number = 10
-  let b: string
+  // Tipos
+  const batman: string = 'Bruce'
+  const superman: string = 'Clark'
+  const existe: boolean = false
+  console.log(batman, superman, existe)
 
-  console.log(a)
+  // Tuplas
+  const parejaHeroes: [string, string] = [batman, superman]
+  const villano: [string, number, boolean] = ['Lex Lutor', 5, true]
+  console.log({ parejaHeroes, villano })
+
+  // Arreglos
+  const aliados: string[] = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash']
+  console.log({ aliados })
+
+  //Enumeraciones
+  enum fuerza {
+    acuaman = 0,
+    batman = 1,
+    flash = 5,
+    superman = 100,
+  }
+  const fuerzaFlash = fuerza.flash
+  const fuerzaSuperman = fuerza.superman
+  const fuerzaBatman = fuerza.batman
+  const fuerzaAcuaman = fuerza.acuaman
+  console.log(fuerza)
+
+  // Retorno de funciones
+  function activar_batiseñal(): string {
+    return 'activada'
+  }
+  console.log(activar_batiseñal())
+
+  function pedir_ayuda(): void {
+    console.log('Auxilio!!!')
+  }
+  console.log(pedir_ayuda())
+
+  // Aserciones de Tipo
+  const poder: any = '100'
+  const largoDelPoder: number = (poder as string).length
+  const largoDelPoder1: number = (<string>poder).length
+  console.log({ poder, largoDelPoder, largoDelPoder1 })
 })()
