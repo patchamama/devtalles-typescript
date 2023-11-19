c68c49b6a173e3b531bef5a6d16e0196b24851c4
A. Urquiola <patchamama.go@gmail.com>
2023-11-19 22:29:29 +0100

    Resolución de la tarea - Desestructuración

---
+++ typescript-intro/src/topics/06-function-destructuring.ts
@@ -0,0 +1,41 @@
+// Function destructuring
+
+interface Product {
+  description: string
+  price: number
+}
+
+const phone: Product = {
+  description: 'Nokia A1',
+  price: 150.0,
+}
+
+const tablet: Product = {
+  description: 'iPad Air',
+  price: 350.0,
+}
+
+interface TaxCalculationOptions {
+  tax: number
+  products: Product[]
+}
+
+// function taxCalculation(options: TaxCalculationOptions): [number, number] {
+function taxCalculation(options: TaxCalculationOptions): [number, number] {
+  const { tax, products } = options
+  let total = 0
+  products.forEach(({ price }) => {
+    total += price
+  })
+  return [total, total * tax]
+}
+
+const shoppingCart = [phone, tablet]
+const tax = 0.15
+
+const [total, taxResult] = taxCalculation({ tax, products: shoppingCart })
+
+console.log(`Total: ${total}`)
+console.log(`Tax: ${taxResult}`)
+
+export {}
