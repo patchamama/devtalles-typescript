eb3247bf86755fdf8cf64dcef63c035c0f80165d
A. Urquiola <patchamama.go@gmail.com>
2023-11-19 20:31:15 +0100

    Objetos, arreglos e interfaces

--- typescript-intro/src/main.ts
+++ typescript-intro/src/main.ts
@@ -3,7 +3,8 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 // import { setupCounter } from './counter'
 
-import './topics/01-basic-types'
+// import './topics/01-basic-types'
+import './topics/02-object-interface'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola Mundo
