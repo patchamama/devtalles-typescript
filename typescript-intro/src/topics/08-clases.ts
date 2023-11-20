76e2c22e3bd4b8eaa9a5a33afa2fe6b7fe2583ae
A. Urquiola <patchamama.go@gmail.com>
2023-11-20 10:02:21 +0100

    Priorizar composición sobre herencia

--- typescript-intro/src/topics/08-clases.ts
+++ typescript-intro/src/topics/08-clases.ts
@@ -18,18 +18,24 @@ export class Person {
   }
 }
 
-export class Hero extends Person {
+export class Hero {
+  public person: Person
+
   constructor(
     public alterEgo: string,
     public age: number,
-    public realName: string
+    public realName: string,
+    public person: Person // public person: Person = new Person(alterEgo, 'New York')
   ) {
-    super(alterEgo, 'New York') // Llamada al constructor de la clase padre Person con los parámetros
+    // super(alterEgo, 'New York') // Llamada al constructor de la clase padre Person con los parámetros
+    // this.person = new Person(alterEgo, 'New York')
   }
 }
 
-const ironman = new Hero('Tony Stark', 45, 'Tony')
-console.log(ironman.getName())
-console.log(ironman.getAddress())
+const bruce = new Person('Bruce', 'New York')
+
+const ironman = new Hero('Tony Stark', 45, 'Tony', bruce)
+console.log(ironman.person.getName())
+console.log(ironman.person.getAddress())
 console.log(ironman)
 // console.log(ironman.address) // Error: Property 'address' is private and only accessible within class 'Person'.
